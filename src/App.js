a17396b111d78223903624b113532e9bd9307945
melania7730 <150779229+melania7730@users.noreply.github.com>
2024-03-26 00:01:10 +0800

    Added Github repo link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Melania and is{" "}
+        <a href="https://github.com/melania7730/react-weather-app-mel">
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
