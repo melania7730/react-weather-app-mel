2918b8a4f66c5d9deb8be4b0e2696c395596b255
melania7730 <150779229+melania7730@users.noreply.github.com>
2024-03-27 15:34:28 +0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Melania and is{" "}
-        <a href="https://github.com/melania7730/react-weather-app-mel">
-          {" "}
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Melania and is{" "}
+          <a href="https://github.com/melania7730/react-weather-app-mel">
+            {" "}
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
