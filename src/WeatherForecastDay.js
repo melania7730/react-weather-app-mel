7b36c2d72540e3f96ef4e6bf2bf3f82265d23fd5
melania7730 <150779229+melania7730@users.noreply.github.com>
2024-04-17 14:07:37 +0700

    Displayed 5 days weather forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -20,7 +20,7 @@ export default function WeatherForecastDay(props) {
     <div>
       {" "}
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={32} />{" "}
+      <WeatherIcon code={props.data.weather[0].icon} size={40} />{" "}
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
