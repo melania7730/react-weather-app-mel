1382429d2775b272f613a04f819f7ec43885a9b2
melania7730 <150779229+melania7730@users.noreply.github.com>
2024-04-18 15:34:02 +0700

    Integrated 5 days Weather Forecast to API

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -20,7 +20,7 @@ export default function WeatherForecastDay(props) {
     <div>
       {" "}
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={40} />{" "}
+      <WeatherIcon code={props.data.weather[0].icon} size={65} />{" "}
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
